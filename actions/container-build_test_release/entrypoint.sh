870acc1d86dd51f2dd72f0715ad57e654b5e114e
yzhang <yzhang23@iastate.edu>
2023-08-21 16:02:46 -0500

    add testing to the container action

--- actions/container-build_test_release/entrypoint.sh
+++ actions/container-build_test_release/entrypoint.sh
@@ -17,7 +17,13 @@ cd /docker-action
 
 echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
 
-docker build -t docker-action --build-arg btag=$BASE_TAG --build-arg gcc_version=$GCC_VRESION --build-arg clang_version=$CLANG_VERSION --build-arg ninja_build=$NINJA_BUILD --build-arg use_clang=$USE_CLANG --build-arg cmaize_github_token=$CMAIZE_GITHUB_TOKEN --build-arg install=$INSTALL --build-arg unit_test=$TEST --build-arg int_test=$INTEGRATION_TEST . && docker run docker-action -v /install:/install
+docker build -t docker-action --build-arg btag=$BASE_TAG --build-arg gcc_version=$GCC_VRESION --build-arg clang_version=$CLANG_VERSION --build-arg ninja_build=$NINJA_BUILD --build-arg use_clang=$USE_CLANG --build-arg cmaize_github_token=$CMAIZE_GITHUB_TOKEN --build-arg install=$INSTALL --build-arg unit_test=$TEST --build-arg int_test=$INTEGRATION_TEST . && docker run docker-action 
+
+container_id="$(docker container ls  | grep 'docker-action')"
+container_id="${container_id%% *}"
+echo "container_id is " ${container_id}
+docker cp -r container_id:/install /install
+ls /install
 
 cat <<EOF>> release.Dockerfile
 FROM ubuntu:20.04
